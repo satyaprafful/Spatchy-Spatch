0a363ae3e0b00b971239b89a09b9a77d3ff97904
ttasya <tanya.tspe@gmail.com>
2020-04-27 22:16:22 -0400

    Added expanding rows to IngrSearch

--- client/src/components/IngrSearch.js
+++ client/src/components/IngrSearch.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageNavbar from './PageNavbar';
 import RecipeRow from './RecipeRow';
+import {Container, Row, Col, Accordion, Card, Button, Form} from 'react-bootstrap';
 
   const divStyle = {
       display: 'flex',
@@ -35,17 +36,6 @@ export default class IngrSearch extends React.Component {
 
     }
 
-//  getIngrList()
-//  {
-//    var ingr1 = 1;
-//    var ingr2 = 2;
-//    var ingr3 = 3;
-//    var ingr4 = 4;
-//    var ingr5 = 5;
-//
-//    return "${ingr1}&${ingr2}&${ingr3}&${ingr4}&${ingr5}";
-//  }
-
   parseIngrList()
   {
     var ingrList = this.state.ingrList;
@@ -74,10 +64,66 @@ export default class IngrSearch extends React.Component {
 
         console.log(recipesList); //delete this
 
-        let recipesDiv = recipesList.map((recipeObj, i) =>
-          <RecipeRow title = {recipeObj.title} key= {recipeObj.rID} />
-        );
+//        let recipesDiv = recipesList.map((recipeObj, i) =>
+//          <RecipeRow title = {recipeObj.title} key= {recipeObj.rID} />
+//        );
 
+        let recipesDiv = recipesList.map((recipe, i) => {
+            var raw = recipe.ingr_descr;
+            var ingrArr = raw.substring(1,raw.length-1).split(",")
+
+            let ingrDivs = ingrArr.map((tuple, i) => {
+                var curr = ingrArr[i];
+                curr = curr.replace(/\'/g, "");
+
+                return  <Row>
+                             <Col>
+                                  {curr}
+                             </Col>
+                        </Row>
+                }
+            )
+
+
+            return (
+                <Row><Col>
+                    <Accordion>
+                        <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Card.Header} eventKey="0">
+                                {(i+1) + ". " + recipe.title}
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>
+                                <Row>
+                                    <Col>
+                                        <strong>Recipe Name:</strong>: {recipe.title}
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                    <strong>Rating:</strong> {recipe.rating}
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                    <strong>Description:</strong> {recipe.recipe_descr}
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <strong>Ingredients:</strong> {ingrDivs}
+                                    </Col>
+                                </Row>
+
+                            </Card.Body>
+                        </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
+                </Col></Row>
+            )
+        });
 
         ///This saves our HTML representation of the data into the state, which we can call in our render function
         this.setState({
@@ -90,16 +136,13 @@ export default class IngrSearch extends React.Component {
   }
 
   componentDidMount() {
-//		const fruitIngrList = [['Apple', 4], ['Banana', 15], ['Peach', 133], ['Strawberry', 179]];
-//		const vegIngrList = [['Avocado', 11], ['Carrot', 36], ['Lettuce', 104], ['Spinach', 177]];
-
 
 //  FULL INGREDIENT LISTS:
 //        const meatIngrList = [['bacon', 12], ['beef', 21], ['chicken', 41], ['chicken breast', 209], ['chicken leg', 42], ['chuck roast', 48], ['ground beef', 213], ['ground chuck', 85], ['ham', 88], ['lamb', 99], ['pork', 141], ['prosciutto', 220], ['rib roast', 151], ['round roast', 221], ['sausage', 163], ['short rib', 169], ['steak', 176], ['turkey', 188], ['veal', 191], ['anchovy', 2],['bass', 17], ['clam', 210], ['fish', 73], ['salmon', 161], ['scallop', 165], ['shrimp', 171], ['tuna', 187]];
 //        const vegIngrList = [['artichoke', 6], ['arugula', 7], ['asparagus', 9], ['avocado', 11], ['bean', 20], ['beet', 206], ['blueberry', 23], ['broccoli', 27], ['cabbage', 31], ['carrot', 36], ['cauliflower', 207], ['celery', 38], ['corn', 58], ['cucumber', 63], ['garlic', 76], ['greens', 84], ['jalapeño', 96], ['kale', 215], ['lemon', 100], ['lentil', 101], ['lettuce', 102], ['lime', 103], ['mushroom', 115], ['olive', 120], ['onion', 121], ['pea', 131], ['pepper', 137], ['potato', 143], ['pumpkin', 146], ['radish', 147], ['scallion', 164], ['shallot', 167], ['spinach', 175], ['tomato', 186], ['zucchini', 202]];
-        const fruitIngrList = [['apple', 4], ['apricot', 5], ['banana', 15], ['cherry', 40], ['cranberry', 61], ['currant', 65], ['grape', 83], ['mango', 216], ['orange', 123], ['peach', 132], ['pear', 134], ['raspberry', 149], ['strawberry', 177]];
+//        const fruitIngrList = [['apple', 4], ['apricot', 5], ['banana', 15], ['cherry', 40], ['cranberry', 61], ['currant', 65], ['grape', 83], ['mango', 216], ['orange', 123], ['peach', 132], ['pear', 134], ['raspberry', 149], ['strawberry', 177]];
 //        const dairyIngrList = [['asiago', 8], ['blue cheese', 22], ['burrata', 29], ['butter', 30], ['camembert', 32], ['cheddar', 39], ['cheddar cheese', 208], ['colby', 55], ['cottage cheese', 59], ['cream cheese', 62], ['crème fraîche', 212], ['feta', 72], ['fontina', 75], ['goat cheese', 80], ['gorgonzola', 81], ['gouda', 82], ['gruyère', 86], ['halibut', 87], ['havarti', 89], ['heavy cream', 90], ['ice cream', 94], ['margarine', 107], ['mascarpone', 108], ['milk', 110], ['monterey jack', 113], ['mozzarella', 114], ['parmesan', 128], ['provolone', 144], ['ricotta', 153], ['romano cheese', 156], ['sour cream', 172], ['swiss', 179], ['whipped cream', 196], ['yogurt', 201]];
-        const grainIngrList = [['baguette', 204], ['bread', 26], ['couscous', 211], ['cracker', 60], ['linguini', 104], ['macaroni', 105], ['noodle', 117], ['oats', 217], ['pasta', 130], ['penne', 136], ['ravioli', 150], ['rice', 152], ['rigatoni', 154], ['spaghetti', 174]];
+//        const grainIngrList = [['baguette', 204], ['bread', 26], ['couscous', 211], ['cracker', 60], ['linguini', 104], ['macaroni', 105], ['noodle', 117], ['oats', 217], ['pasta', 130], ['penne', 136], ['ravioli', 150], ['rice', 152], ['rigatoni', 154], ['spaghetti', 174]];
 //        const otherIngrList = [['almond', 1], ['apple sauce', 203], ['beer', 205], ['bourbon', 24], ['broth', 28], ['capers', 33], ['chocolate', 46], ['cocoa', 52], ['coconut', 53], ['coffee', 54], ['cookie', 56], ['curry', 66], ['egg', 68], ['gelatin', 77], ['gin', 78], ['hazelnut', 214], ['honey', 92], ['jelly', 97], ['maple syrup', 106], ['peanut', 133], ['pecan', 135], ['phyllo pastry', 138], ['pine nut', 218], ['potato chip', 219], ['puff pastry', 145], ['raisin', 148], ['rum', 159], ['sherry', 168], ['vodka', 193], ['walnut', 194], ['wine', 198]];
 
 
@@ -108,7 +151,8 @@ export default class IngrSearch extends React.Component {
         const meatIngrList = [['bacon', 12], ['beef', 21], ['chicken', 41], ['chicken breast', 209], ['chicken leg', 42], ['chuck roast', 48], ['ground beef', 213], ['ground chuck', 85], ['ham', 88], ['lamb', 99], ['pork', 141], ['prosciutto', 220], ['rib roast', 151]];
         const dairyIngrList = [['asiago', 8], ['blue cheese', 22], ['burrata', 29], ['butter', 30], ['camembert', 32], ['cheddar', 39], ['cheddar cheese', 208], ['colby', 55], ['cottage cheese', 59], ['cream cheese', 62], ['crème fraîche', 212], ['feta', 72], ['fontina', 75]];
         const otherIngrList = [['almond', 1], ['apple sauce', 203], ['beer', 205], ['bourbon', 24], ['broth', 28], ['capers', 33], ['chocolate', 46], ['cocoa', 52], ['coconut', 53], ['coffee', 54], ['cookie', 56], ['curry', 66], ['egg', 68], ['gelatin', 77]];
-
+        const fruitIngrList = [['apple', 4], ['apricot', 5], ['banana', 15], ['cherry', 40], ['cranberry', 61], ['currant', 65], ['grape', 83], ['mango', 216], ['orange', 123], ['peach', 132], ['pear', 134], ['raspberry', 149], ['strawberry', 177]];
+        const grainIngrList = [['baguette', 204], ['bread', 26], ['couscous', 211], ['cracker', 60], ['linguini', 104], ['macaroni', 105], ['noodle', 117], ['oats', 217], ['pasta', 130], ['penne', 136], ['ravioli', 150], ['rice', 152], ['rigatoni', 154], ['spaghetti', 174]];
 
 		let fruitDivs = fruitIngrList.map((tuple, i) => {
             return  <div>
@@ -169,70 +213,63 @@ export default class IngrSearch extends React.Component {
 		return (
 			<div className="IngrSearch">
 				<PageNavbar active="Ingredient based searching" />
-
-			    <div className="container ingredients-container">
-			    	<div className="jumbotron">
-			    		<div className="h1">IngrSearch</div>
-			    		<div className="input-container">
-                            <div className='ingrRow' >
-                                <legend><h3>Choose your ingredients: </h3> </legend>
-
-                                        <div className='ingrOptions' style={divStyle} >
-                                            <div className='fruitOptions'>
-                                                <div class="divider"  /> <h5> Fruit </h5>
-                                                    <div className="fruit-ingr">
-                                                        {this.state.fruitDivs}
-                                                    </div>
-                                            </div>
-                                            <div className='vegOptions' >
-                                                <div class="divider" /> <h5> Vegetables </h5>
-                                                    <div className="veg-ingr">
-                                                        {this.state.vegDivs}
-                                                    </div>
-                                            </div>
-                                            <div className='meatOptions' >
-                                                <div class="divider" /> <h5> Meat/Fish </h5>
-                                                    <div className="meat-ingr">
-                                                        {this.state.meatDivs}
-                                                    </div>
-
-                                            </div>
-                                            <div className='dairyOptions' >
-                                                <div class="divider" /> <h5> Dairy </h5>
-                                                    <div className="dairy-ingr">
-                                                        {this.state.dairyDivs}
-                                                    </div>
-                                          </div>
-                                          <div className='grainOptions' >
-                                                <div class="divider" /> <h5> Grain </h5>
-                                                    <div className="grain-ingr">
-                                                        {this.state.grainDivs}
-                                                    </div>
-                                          </div>
-                                          <div className='otherOptions' >
-                                                <div class="divider" /> <h5> Other </h5>
-                                                    <div className="other-ingr">
-                                                        {this.state.otherDivs}
-                                                    </div>
-                                                </div>
-                                            </div>
+                <br></br>
+			<Container>
+			    <div className="h1">IngrSearch</div>
+                    <legend><h3>Choose your ingredients: </h3> </legend>
+                        <div className='ingrOptions' style={divStyle} >
+                            <div className='fruitOptions'>
+                                <div class="divider"  /> <h5> Fruit </h5>
+                                    <div className="fruit-ingr">
+                                        {this.state.fruitDivs}
+                                    </div>
                             </div>
-                            <br></br>
-			    			<button id="submitBtn" className="submit-btn" onClick={this.submitIngr}>Submit</button>
-			    		</div>
-			    		<br></br>
-			    		<div className="header-container">
-			    			<div className="h6">Here are some suggested recipes ...</div>
-			    			<div className="headers">
-			    				<div className="header"><strong>Title</strong></div>
-			    			</div>
-			    		</div>
-			    		<div className="results-container" id="results">
-			    			{this.state.recRecipes}
-			    		</div>
-			    	</div>
-			    </div>
-		    </div>
+                            <div className='vegOptions' >
+                                <div class="divider" /> <h5> Vegetables </h5>
+                                    <div className="veg-ingr">
+                                        {this.state.vegDivs}
+                                    </div>
+                            </div>
+                            <div className='meatOptions' >
+                                <div class="divider" /> <h5> Meat/Fish </h5>
+                                    <div className="meat-ingr">
+                                        {this.state.meatDivs}
+                                    </div>
+
+                            </div>
+                            <div className='dairyOptions' >
+                                <div class="divider" /> <h5> Dairy </h5>
+                                    <div className="dairy-ingr">
+                                        {this.state.dairyDivs}
+                                    </div>
+                          </div>
+                          <div className='grainOptions' >
+                                <div class="divider" /> <h5> Grain </h5>
+                                    <div className="grain-ingr">
+                                        {this.state.grainDivs}
+                                    </div>
+                          </div>
+                          <div className='otherOptions' >
+                                <div class="divider" /> <h5> Other </h5>
+                                    <div className="other-ingr">
+                                        {this.state.otherDivs}
+                                    </div>
+                                </div>
+                            </div>
+                        <br></br>
+			    	    <button id="submitBtn" className="submit-btn" onClick={this.submitIngr}>Submit</button>
+                        <br></br>
+                    <div className="header-container">
+                        <div className="h6">Here are some suggested recipes ...</div>
+                        <div className="headers">
+                            <div className="header"><strong>Title</strong></div>
+                        </div>
+                    </div>
+                    <div className="results-container" id="results">
+                        {this.state.recRecipes}
+                    </div>
+			    </Container>
+			</div>
 		);
 	}
 
